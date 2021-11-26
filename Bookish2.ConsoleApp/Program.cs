9822ea0faf31f6bcc0d3a2127514c549b0db8d0b
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2021-11-26 11:00:25 +0000

    Initial commit

---
+++ Bookish2.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Bookish2.DataAccess;
+
+namespace Bookish2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            LibraryDatabaseClient.GetAllBooks();
+            Console.WriteLine();
+            LibraryDatabaseClient.GetAllLoans(1);
+            Console.WriteLine();
+            LibraryDatabaseClient.Search("am");
+            Console.WriteLine();
+            LibraryDatabaseClient.CheckAvailability(4);
+        }
+    }
+}
---
+++ Bookish2.DataAccess/Author.cs
@@ -0,0 +1,9 @@
+﻿namespace Bookish2.DataAccess
+{
+    public class Author
+    {
+        public int AuthorId;
+        public string FirstName;
+        public string LastName;
+    }
+}
